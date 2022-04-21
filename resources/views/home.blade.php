68e63606b9929b7cd2fd0b75d90a37043f106d31
matteopelusi7 <pelusimatteo02@gmail.com>
2022-04-21 17:41:44 +0200

    Add style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,36 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Movies</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
 
-    <div>
+    <div class="container">
 
         @foreach ($movies as $movie)
 
-            <div>
+            <div class="card-item">
                 <h3>Titolo:</h3>
                 <p>{{ $movie['title'] }}</p>
-            </div>
-            <div>
                 <h3>Titolo originale:</h3>
                 <p>{{ $movie['original_title'] }}</p>
-            </div>
-            <div>
                 <h3>Nazionalità:</h3>
-                <p>{{ $movie['nazionality'] }}</p>
-            </div>
-            <div>
+                <p>{{ $movie['nationality'] }}</p>
                 <h3>Data:</h3>
                 <p>{{ $movie['date'] }}</p>
-            </div>
-            <div>
                 <h3>Voto:</h3>
                 <p>{{ $movie['vote'] }}</p>
             </div>
             
         @endforeach
-        
+
     </div>
     
 </body>
